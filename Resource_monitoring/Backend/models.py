d298e40cf9d0fb0ad6ff3d3b7ebd71803c48be1f
Rishabh Nahar <rishabhnahar1202@gmail.com>
2022-08-24 00:00:58 +0530

    initiated models

--- Resource_monitoring/Backend/models.py
+++ Resource_monitoring/Backend/models.py
@@ -1,7 +1,34 @@
+from pyexpat import model
 from django.db import models
+import uuid
 
 # Create your models here.
-class test(models.model):
-    col1 = models.CharField(max_length=30)
+class alerts(models.Model):
+    id = models.AutoField(primary_key=True)
+    session_id = models.ForeignKey("session_data", verbose_name="Session_id", on_delete=models.CASCADE)
+    temperature = models.FloatField()
+    humidity = models.FloatField()
+    temperature_in_range = models.BooleanField()
+    humidity_in_range = models.BooleanField()
+     
     class Meta:
-        db_table = "test_table"
+        db_table = "Alerts"
+    
+
+class session_data(models.Model):
+    session_ID = models.CharField(max_length=10,primary_key=True)
+    numbers = models.IntegerField()
+    serial = models.CharField(max_length=20)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    session_start_time = models.CharField(max_length= 15)
+    session_end_time = models.CharField(max_length=15)
+    session_status = models.BooleanField()
+    temp_highest_level = models.FloatField()
+    temp_lowest_level = models.FloatField()
+    humidity_highest_level = models.FloatField()
+    humidity_lowest_level = models.FloatField()
+    time_interval = models.TimeField(default= "00:00:00")
+
+    class Meta:
+        db_table = "Session_data"
+
