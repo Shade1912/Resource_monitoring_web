8a47337b096fc5646f7464f77f75fdd6d18bc7ec
VatsalHShah <svatsal86@gmail.com>
2022-08-24 00:20:49 +0530

    database first trial

--- Resource_monitoring/Resource_monitoring/settings.py
+++ Resource_monitoring/Resource_monitoring/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'resource_monitoring_web',
         'USER':'root',
-        'PASSWORD':'',
+        'PASSWORD':os.environ['mysql_password'],
     }
 }
 
