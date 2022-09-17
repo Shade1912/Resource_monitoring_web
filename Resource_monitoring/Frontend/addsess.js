e7cd7ef6b99d81ae6ccb9870a46d50352aec9d43
nidan2110 <nidangavali1@gmail.com>
2022-09-17 20:22:13 +0530

    all templates

--- Resource_monitoring/Frontend/addsess.js
+++ Resource_monitoring/Frontend/addsess.js
@@ -8,13 +8,13 @@ function validate()
     const form = document.getElementById('addsessform')
 
         if (maxt.value <= mint.value){
-            window.alert("Max value is smaller than min value");
+            window.alert("Max temperature value is smaller than min temperature value");
             maxt.focus();
             return false;
         }
 
         if (maxh.value <= minh.value){
-            window.alert("Max value is smaller than min value");
+            window.alert("Max humidity value is smaller than min humididty value");
             maxh.focus();
             return false;
         }
@@ -22,5 +22,13 @@ function validate()
             true;
         }
         
-    
+    let newArray = [maxt,maxh,mint,minh];
+    newArray.forEach((item)=>{
+        var li = document.createElement("li");
+    var text = document.createElement(item);
+    li.appendChild(text);
+    table.appendChild(li);    
+    })
+    form.reset();
 }
+
