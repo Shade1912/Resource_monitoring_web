2e6f11475d69ea5ab0441956c28e53eb4ecf8baa
nidan2110 <nidangavali1@gmail.com>
2022-11-19 18:23:29 +0530

    Monitoring UI

--- Resource_monitoring/baseapp/views.py
+++ Resource_monitoring/baseapp/views.py
@@ -183,6 +183,9 @@ def dashboard(request):
     # Sessionkey => (request.session._session_key)
     return render(request,"./Dashboard.html")
 
+def monitor(request):
+    return render(request,"./Monitoring.html")    
+
 def addSession(request):
     random_number = random.randint(0,500)
     timestamp = datetime.now()  
