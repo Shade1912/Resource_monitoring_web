bc68a2e3c20049bb60e52c28f2f9ac730c9f2c53
Rishabh Nahar <rishabhnahar1202@gmail.com>
2023-03-23 19:46:10 +0530

    test page

--- Resource_monitoring/Resource_monitoring/settings.py
+++ Resource_monitoring/Resource_monitoring/settings.py
@@ -83,7 +83,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'resource_monitoring_web',
         'USER':'root',
-        'PASSWORD':'',
+        # 'PASSWORD':'',
+        'PASSWORD':'Qwerty@21',
     }
 }
 
