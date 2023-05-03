c6c2b6931caf2508197c48cf8a0ee57cc61e4aa5
nidan2110 <nidangavali1@gmail.com>
2023-05-03 16:44:52 +0530

    commit

--- Resource_monitoring/baseapp/admin.py
+++ Resource_monitoring/baseapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
 
 # Register your models here.
+from baseapp.models import alerts,session_data,group_privileges,user_extra_details,userlogs,drug_template,device,probe;
